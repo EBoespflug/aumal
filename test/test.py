e64affaf9f49368a562bda9de27adfea8d7dc95c
Etienne Boespflug <etienne.boespflug@gmail.com>
2018-11-23 16:26:53 +0100

    Ended aliasing part.

--- test/test.py
+++ test/test.py
@@ -5,13 +5,20 @@ from FA import *
 
 a = FA("abc")
 
-a.add_state("0", True)
+a.add_state("0")
 a.add_state("1")
 a.add_state("2")
-a.add_state("3")
+a.add_state("3", True)
 a.init = "0"
 
-a.add_transition("0", "a", "3")
-a.add_transition("0", "b", "3")
+a.add_transition("0", "a", "1")
+a.add_transition("0", "b", "0")
+a.add_transition("1", "a", "1")
+a.add_transition("1", "b", "2")
+a.add_transition("2", "a", "3")
+a.add_transition("2", "b", "0")
+a.add_transition("3", "a", "3")
+a.add_transition("3", "b", "3")
+
 
 print(a)
